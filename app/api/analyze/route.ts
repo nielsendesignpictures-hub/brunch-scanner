feb4405a287cbfab450c4d9c422805ed4029b820
nielsendesignpictures-hub <nielsendesignpictures@gmail.com>
2026-05-18 13:00:24 +0200

    Update route.ts

--- app/api/analyze/route.ts
+++ app/api/analyze/route.ts
@@ -2,34 +2,33 @@ import { NextResponse } from 'next/server'
 import Jimp from 'jimp'
 
 const menuItems = [
-  // MEJERI
-  { name: 'Røræg', x: 0.07, y: 0.24 },
-  { name: 'Spejlæg', x: 0.07, y: 0.29 },
-  { name: 'Modnet Havarti ost', x: 0.07, y: 0.35 },
-  { name: 'Hjemmelavet blåbæryoghurt', x: 0.07, y: 0.41 },
-  { name: 'Hjemmelavet chiagrød', x: 0.07, y: 0.49 },
+ // MEJERI
+{ name: 'Røræg', x: 0.06, y: 0.19 },
+{ name: 'Spejlæg', x: 0.06, y: 0.25 },
+{ name: 'Modnet Havarti ost', x: 0.06, y: 0.31 },
+{ name: 'Hjemmelavet blåbæryoghurt', x: 0.06, y: 0.37 },
+{ name: 'Hjemmelavet chiagrød', x: 0.06, y: 0.46 },
 
-  // PLANTERIGET
-  { name: 'Avocado og hytteost', x: 0.07, y: 0.66 },
-  { name: 'Eksotisk frugtskål', x: 0.07, y: 0.72 },
-  { name: 'Smashed peas med forårsmynte', x: 0.07, y: 0.77 },
+// PLANTERIGET
+{ name: 'Avocado og hytteost', x: 0.06, y: 0.61 },
+{ name: 'Eksotisk frugtskål', x: 0.06, y: 0.67 },
+{ name: 'Smashed peas med forårsmynte', x: 0.06, y: 0.73 },
 
-  // BAGERIET
-  { name: 'Pain au chocolat fra Meyers', x: 0.07, y: 0.92 },
-  { name: 'Øko. smørcrossaint fra Meyers', x: 0.07, y: 0.98 },
+// BAGERIET
+{ name: 'Pain au chocolat fra Meyers', x: 0.06, y: 0.87 },
+{ name: 'Øko. smørcrossaint fra Meyers', x: 0.06, y: 0.93 },
 
-  // KØD OG FISK
-  { name: 'Rösti', x: 0.55, y: 0.24 },
-  { name: 'Hjemmelavet hønsesalat', x: 0.55, y: 0.30 },
-  { name: 'Crispy chicken', x: 0.55, y: 0.38 },
-  { name: '2 brunchpølser', x: 0.55, y: 0.46 },
-  { name: 'Koldrøget laks', x: 0.55, y: 0.54 },
+// KØD OG FISK
+{ name: 'Rösti', x: 0.54, y: 0.19 },
+{ name: 'Hjemmelavet hønsesalat', x: 0.54, y: 0.26 },
+{ name: 'Crispy chicken', x: 0.54, y: 0.34 },
+{ name: '2 brunchpølser', x: 0.54, y: 0.42 },
+{ name: 'Koldrøget laks', x: 0.54, y: 0.51 },
 
-  // FINALEN
-  { name: 'Lun rabarber crumble', x: 0.55, y: 0.66 },
-  { name: 'Øllebrød med let vaniljeskum', x: 0.55, y: 0.74 },
-  { name: '2 amerikanske pandekager', x: 0.55, y: 0.83 },
-]
+// FINALEN
+{ name: 'Lun rabarber crumble', x: 0.54, y: 0.63 },
+{ name: 'Øllebrød med let vaniljeskum', x: 0.54, y: 0.72 },
+{ name: '2 amerikanske pandekager', x: 0.54, y: 0.81 },
 
 export async function POST(req: Request) {
   try {
